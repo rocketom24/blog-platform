4af1d5d64860420bd8850f84a940777784cd5077
rocketom24 <tasmimdead@gmail.com>
2025-03-26 13:36:48 +0600

    Configure MongoDB connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    MongooseModule.forRoot(
+      process.env.MONGO_URI ||
+        'mongodb+srv://tasmimdead:mangotas@cluster0.ead5r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    ),
   ],
   controllers: [AppController],
   providers: [AppService],
